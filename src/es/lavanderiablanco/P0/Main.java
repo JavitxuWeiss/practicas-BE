44bcd120f067f758b46b59496ed78b9787e47f00
Javier Blanco <javitxuweiss@gmail.com>
2020-04-28 07:11:47 +0200

    first commit

---
+++ src/es/lavanderiablanco/P0/Main.java
@@ -0,0 +1,11 @@
+package es.lavanderiablanco.P0;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hola CHavales!!!uno"); //esto es un comentario de linea
+	System.out.println("Hola CHavales!!!dos");   /* esto es un comentario de bloque */
+    System.out.println("Hola CHavales!!!tres");  /** esto es un comentario de javaDoc -- para mas info:"http://bit.ly/psjavado" */
+    }
+}
